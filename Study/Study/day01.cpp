628b289497440cc252a9be0ba160c2d535257c77
panyi <525647740@qq.com>
2015-07-07 21:07:06 +0800

    Hello World提交

---
+++ Study/Study/day01.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Study
+//
+//  Created by  潘易  on 15/7/7.
+//  Copyright (c) 2015年 xinlan. All rights reserved.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout 标准输出  操作符重载
+    //endl  = "\n"
+    cout<<"Hello......"<<endl;
+    
+    return 0;
+}//end main
